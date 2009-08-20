d8d578fe2c536efd33d16f0505b7de65a2cbb3be
Bernardo Heynemann <heynemann@gmail.com>
2009-08-19 22:50:56 -0300

    GenerateManaAndTap ability tests.

--- tests/unit/test_land_abilities.py
+++ tests/unit/test_land_abilities.py
@@ -54,3 +54,61 @@ def test_generate_mana_and_tap_ability_raises_when_card_is_tapped():
 
     assert_raises(InvalidOperationError, land.GenerateManaAndTap, exc_pattern=r"The player can't generate mana out of a tapped card.")
 
+def test_generate_mana_and_tap_ability_generates_one_mana():
+    new_game = Game()
+
+    bernardo = Player(name="Bernardo", deck=deepcopy(data.green_land_deck))
+    john = Player(name="John", deck=deepcopy(data.black_land_deck))
+    new_game.add_player(bernardo)
+    new_game.add_player(john)
+
+    new_game.initialize()
+
+    land = bernardo.position.hand[0]
+    bernardo.play(land)
+
+    land.GenerateManaAndTap()
+
+    assert bernardo.position.mana["green"] == 1
+
+def test_generate_mana_and_tap_ability_taps_card():
+    new_game = Game()
+
+    bernardo = Player(name="Bernardo", deck=deepcopy(data.green_land_deck))
+    john = Player(name="John", deck=deepcopy(data.black_land_deck))
+    new_game.add_player(bernardo)
+    new_game.add_player(john)
+
+    new_game.initialize()
+
+    land = bernardo.position.hand[0]
+    bernardo.play(land)
+
+    land.GenerateManaAndTap()
+
+    assert land.is_tapped
+
+def test_generate_mana_and_tap_ability_publishes_mana_generated_event():
+    new_game = Game()
+    messages = []
+
+    def handle_event(game, position, card):
+        messages.append("Card %s tapped to generate 1 %s mana" % (card.name, card.color))
+
+    new_game.bus.subscribe('mana_generated', handle_event)
+
+    bernardo = Player(name="Bernardo", deck=deepcopy(data.green_land_deck))
+    john = Player(name="John", deck=deepcopy(data.black_land_deck))
+    new_game.add_player(bernardo)
+    new_game.add_player(john)
+
+    new_game.initialize()
+
+    land = bernardo.position.hand[0]
+    bernardo.play(land)
+
+    land.GenerateManaAndTap()
+
+    assert len(messages) == 1
+    assert messages[0] == "Card Forest tapped to generate 1 green mana"
+
